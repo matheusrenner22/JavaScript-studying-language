56d0a26a2d94ae50b5c404e581445704904d0e09
FoxTrojan <matheusrennerdev@outlook.com>
2022-11-08 09:51:01 -0300

    update setTimeout study

--- 13-assincrono-e-promisses/03-setTimeout.js
+++ 13-assincrono-e-promisses/03-setTimeout.js
@@ -3,3 +3,12 @@
 setTimeout(function sayHello() {
     console.log('Hello World');
 }, 1000);
+
+
+// setTimeout com função criada externamente
+
+function sayHello2() {
+    console.log('Hello World 2');
+}
+
+setTimeout(sayHello2, 2000);
